eae19e1028f491ba8b58effc00a6be0050382363
Margie YNM <margie.ynm@gmail.com>
2018-04-30 22:52:01 -0700

    add bmiDescription

--- app/src/main/java/com/example/yatusabes/bmicalculator/OutputActivity.java
+++ app/src/main/java/com/example/yatusabes/bmicalculator/OutputActivity.java
@@ -13,6 +13,8 @@ public class OutputActivity extends AppCompatActivity {
         setContentView(R.layout.activity_output);
         showOutput();
     }
+
+
     
     private void showOutput() {
         
@@ -22,5 +24,21 @@ public class OutputActivity extends AppCompatActivity {
 
         TextView answerField = (TextView) findViewById(R.id.bmi_result);
         answerField.setText(bmi.toString());
+
+        TextView descriptionField = (TextView) findViewById(R.id.bmi_description);
+        descriptionField.setText(bmiDescription(bmi));
+    }
+
+    private String bmiDescription(double bmi) {
+        if (bmi < 18.5) {
+            return "Underweight BMI";
+        } else if ((bmi >= 18.5) && (bmi <= 25.0)) {
+            return "Normal BMI";
+        } else if ((bmi > 25 ) && (bmi <=30)) {
+            return "Overweight BMI";
+        } else {
+            return "Obese BMI";
+        }
+
     }
 }
